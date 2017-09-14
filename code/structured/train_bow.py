4ce90203b3e0b296bddade2deec9b28b9bf38673
wboag <william.george.boag@gmail.com>
2017-09-14 09:27:24 -0400

    prediction framework for bow. predicts gender 75%.  needs debugging

---
+++ code/structured/train_bow.py
@@ -0,0 +1,494 @@
+
+import os, sys
+import re
+import math
+import cPickle as pickle
+import shutil
+import io
+from collections import defaultdict
+import numpy as np
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+import pandas as pd
+import datetime
+
+
+from tools import compute_stats_multiclass
+
+
+
+
+def main():
+
+    mode = sys.argv[1]
+
+    hours_s = sys.argv[2]
+    assert hours_s in ['12', '24', '240000'], hours_s
+    hours = float(hours_s)
+
+    train_notes, train_outcomes = get_data('train', mode)
+    dev_notes  ,   dev_outcomes = get_data('dev'  , mode)
+
+    # vectorize notes
+    train_text_features, df  = extract_features_from_notes(train_notes, hours, df=None)
+    dev_text_features  , df_ = extract_features_from_notes(  dev_notes, hours, df=df)
+    assert df == df_
+
+    # Fit model for each prediction task
+    models = {}
+    #tasks = dev_outcomes.values()[0].keys()
+    #tasks = ['diagnosis']
+    tasks = ['gender']
+    excluded = set(['subject_id'])
+    for task in tasks:
+        if task in excluded:
+            continue
+
+        #task = 'admission_location'
+        #task = 'diagnosis'
+
+        # extract appropriate data
+        train_Y, criteria = filter_task(train_outcomes, task, per_task_criteria=None)
+        train_ids = sorted(train_Y.keys())
+        print 'task:', task
+        print 'N:   ', len(train_Y)
+
+        # vecotrize notes
+        X, vectorizers = vectorize_X(train_ids, train_text_features, vectorizers=None)
+
+        # extract labels into list
+        Y = np.array([train_Y[pid] for pid in train_ids])
+
+        # learn SVM
+        clf = LinearSVC(C=1e1)
+        clf.fit(X, Y)
+
+        model = (criteria, vectorizers, clf)
+        models[task] = model
+
+    for task,model in models.items():
+        criteria, vectorizers, clf = model
+        vect = vectorizers[0]
+
+        # train data
+        train_labels,_ = filter_task(train_outcomes, task, per_task_criteria=criteria)
+        train_ids = sorted(train_labels.keys())
+        train_X,_ = vectorize_X(train_ids, train_text_features, vectorizers=vectorizers)
+        train_Y = np.array([train_labels[pid] for pid in train_ids])
+
+        # dev data
+        dev_labels,_ = filter_task(dev_outcomes, task, per_task_criteria=criteria)
+        dev_ids = sorted(dev_labels.keys())
+        dev_X,_ = vectorize_X(dev_ids, dev_text_features, vectorizers=vectorizers)
+        dev_Y = np.array([dev_labels[pid] for pid in dev_ids])
+
+        with io.StringIO() as out_f:
+            # analysis
+            analyze(vect, clf, criteria, out_f)
+
+            # eval on dev data
+            results(model, train_ids, train_X, train_Y, hours, 'TRAIN', task, out_f)
+            results(model,   dev_ids,   dev_X,   dev_Y, hours, 'DEV'  , task, out_f)
+
+            output = out_f.getvalue()
+        print output
+
+        # error analysis
+        error_analysis(model, dev_ids, dev_notes, dev_text_features, dev_X, dev_Y, hours, 'DEV', task)
+
+        # serialize trained model
+        modelname = '../../models/structured/bow/%s_%s.model' % (mode,task)
+        M = {'criteria':criteria, 'vect':vectorizers, 'clf':clf, 'output':output}
+        with open(modelname, 'wb') as f:
+            pickle.dump(M, f)
+
+
+
+def get_data(datatype, mode):
+    filename = '../../data/structured_%s_%s.pickle' % (mode,datatype)
+    with open(filename, 'rb') as f:
+        X = pickle.load(f)
+        outcomes = pickle.load(f)
+
+    assert sorted(X.keys()) == sorted(outcomes.keys())
+    return X, outcomes
+
+
+
+def extract_features_from_notes(notes, hours, df=None):
+    features_list = {}
+
+    # dummy record (prevent SVM from collparsing to single-dimension pred)
+    features_list[-1] = {'foo':1}
+
+    # compute features
+    for pid,records in notes.items():
+        features = extract_text_features(records, hours)
+        features_list[pid] = features
+
+    # build doc freqs if we need them for training
+    if df is None:
+        df = defaultdict(int)
+        for feats in features_list.values():
+            for w in feats.keys():
+                df[w] += 1
+
+    '''
+    # re-scale note-derived features by document frequency
+    min_df = 3
+    tfidf_text_features = {}
+    for pid,feats in features_list.items():
+        val = {}
+        for f,v in feats.items():
+            if f in df:
+                if df[f] >= min_df:
+                    #val[f] = float(v) / df[f]
+                    #val[f] = float(v) / (math.log(df[f]) + 1)
+                    #val[f] = 1. / (math.log(df[f]) + 1)
+                    val[f] = float(v)
+        tfidf_text_features[pid] = val
+
+    # filter down to topN words
+    N = int(1e10)
+    filtered_text_features = {}
+    for pid,feats in tfidf_text_features.items():
+        ranked = sorted(feats.items(), key=lambda t:t[1])
+        topN = dict(ranked[-N:])
+        filtered_text_features[pid] = topN
+    '''
+    filtered_text_features = features_list
+
+
+    return filtered_text_features, df
+
+
+
+def vectorize_X(ids, text_features, vectorizers=None):
+    # learn vectorizer on training data
+    if vectorizers is None:
+        vect = DictVectorizer()
+        vect.fit(text_features.values())
+        vectorizers = (vect,)
+
+    filtered_features = [ text_features[pid] for pid in ids ]
+
+    vect = vectorizers[0]
+    X = vect.transform(filtered_features)
+
+    return X, vectorizers
+
+
+
+def filter_task(Y, task, per_task_criteria=None):
+
+    # If it's a diagnosis, then only include diagnoses that occur >= 10 times
+    if task == 'diagnosis':
+        if per_task_criteria is None:
+            # count diagnosis frequency
+            counts = defaultdict(int)
+            for y in Y.values():
+                counts[y[task]] += 1
+
+            '''
+            for y,c in sorted(counts.items(), key=lambda t:t[1]):
+                print '%4d %s' % (c,y)
+            exit()
+            '''
+
+            # only include diagnois that are frequent enough
+            diagnoses = {}
+            for y,count in counts.items():
+                if count >= 350:
+                    diagnoses[y] = len(diagnoses)
+
+            # save the "good" diagnoses (to extract same set from dev)
+            per_task_criteria = diagnoses
+        else:
+            diagnoses = per_task_criteria
+
+        # which patients have that diagnosis?
+        ids = [pid for pid,y in Y.items() if (y[task] in diagnoses)]
+
+    elif task == 'gender':
+        if per_task_criteria is None:
+            counts = defaultdict(int)
+            for y in Y.values():
+                counts[y[task]] += 1
+
+            # only include diagnois that are frequent enough
+            genders = {gender:i for i,gender in enumerate(counts.keys())}
+
+            # save the "good" diagnoses (to extract same set from dev)
+            per_task_criteria = genders
+        else:
+            genders = per_task_criteria
+
+        # which patients have that diagnosis?
+        ids = [pid for pid,y in Y.items() if (y[task] in genders)]
+
+    elif task == 'ethnicity':
+        if per_task_criteria is None:
+            counts = defaultdict(int)
+            for y in Y.values():
+                normed = normalize_y(y[task], task)
+                if normed == '**ignore**': continue
+                counts[normed] += 1
+
+            # only include diagnois that are frequent enough
+            races = {race:i for i,race in enumerate(counts.keys())}
+
+            # save the "good" diagnoses (to extract same set from dev)
+            per_task_criteria = races
+        else:
+            races = per_task_criteria
+
+        # which patients have that diagnosis?
+        ids = [pid for pid,y in Y.items() if (y[task] in races)]
+
+    else:
+        print task
+        counts = defaultdict(int)
+        for y in train_Y.values():
+            counts[y] += 1
+        hist = defaultdict(int)
+        for y,count in counts.values():
+            hist[count] += 1
+
+        print 'beep bop boop'
+        exit()
+
+    # return filtered data
+    filtered_Y = {pid:y[task] for pid,y in Y.items()}
+    filtered_normed_Y = {pid:normalize_y(y, task) for pid,y in filtered_Y.items() 
+                          if normalize_y(y,task)!='**ignore**'}
+    Y = {pid:per_task_criteria[y] for pid,y in filtered_normed_Y.items()}
+    Y[-1] = len(per_task_criteria)
+    return Y, per_task_criteria
+
+
+
+def analyze(vect, clf, labels_map, out_f):
+
+    ind2feat =  { i:f for f,i in vect.vocabulary_.items() }
+    labels = [label for label,i in sorted(labels_map.items(), key=lambda t:t[1])]
+
+    # most informative features
+    #"""
+    out_f = sys.stdout
+    out_f.write(unicode(clf.coef_.shape))
+    out_f.write(unicode('\n\n'))
+    num_feats = 10
+    informative_feats = np.argsort(clf.coef_)
+
+    for i,label in enumerate(labels):
+
+        neg_features = informative_feats[i,:num_feats ]
+        pos_features = informative_feats[i,-num_feats:]
+
+        #'''
+        # display what each feature is
+        out_f.write(unicode('POS %s\n' % label))
+        for feat in reversed(pos_features):
+            val = clf.coef_[i,feat]
+            word = ind2feat[feat]
+            if val > 1e-4:
+                out_f.write(unicode('\t%-25s: %7.4f\n' % (word,val)))
+            else:
+                break
+        out_f.write(unicode('NEG %s\n' % label))
+        for feat in reversed(neg_features):
+            val = clf.coef_[i,feat]
+            word = ind2feat[feat]
+            if -val > 1e-4:
+                out_f.write(unicode('\t%-25s: %7.4f\n' % (word,val)))
+            else:
+                break
+        out_f.write(unicode('\n\n'))
+        #'''
+        #exit()
+        #"""
+
+
+
+def results(model, ids, X, Y, hours, label, task, out_f):
+    criteria, vectorizers, clf = model
+    vect = vectorizers[0]
+
+    # for AUC
+    P = clf.decision_function(X)
+
+    train_pred = clf.predict(X)
+
+    out_f.write('%s %s' % (unicode(label),task))
+    out_f.write(unicode('\n'))
+    train_pred = clf.predict(X)
+    compute_stats_multiclass(task, train_pred, P, Y, criteria, out_f)
+    out_f.write(unicode('\n\n'))
+
+
+
+def error_analysis(model, ids, notes, text_features, X, Y, hours, label, task):
+    criteria, vectorizers, clf = model
+    vect = vectorizers[0]
+
+    V = {v:k for k,v in criteria.items()}
+    V[len(V)] = '**wrong**'
+
+    # for confidence
+    P = clf.decision_function(X)
+    pred = clf.predict(X)
+
+    # convert predictions to right vs wrong
+    confidence = {}
+    for i,scores in enumerate(P.tolist()):
+        prediction = pred[i]
+        pid = ids[i]
+        ind = Y[i]
+        confidence[pid] = (scores[ind], scores, prediction, ind)
+
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    taskdir = os.path.join(thisdir, 'output', task)
+    if os.path.exists(taskdir):
+        shutil.rmtree(taskdir)
+    os.mkdir(taskdir)
+
+    def importance(featname, p):
+        if featname in vect.vocabulary_:
+            ind = vect.vocabulary_[featname]
+            return clf.coef_[p,ind]
+        else:
+            return float('-inf')
+
+    # order predictions by confidence
+    for pid,conf in sorted(confidence.items(), key=lambda t:t[1]):
+        if pid == -1: continue
+        if conf[2] == conf[3]:
+            success = ''
+        else:
+            success = '_'
+        filename = os.path.join(taskdir, '%s%s.pred' % (success,pid))
+        with open(filename, 'w') as f:
+            print >>f, ''
+            print >>f, '=' * 80
+            print >>f, ''
+            print >>f, pid
+            print >>f, 'scores:', conf[1]
+            print >>f, 'pred:  ', V[conf[2]]
+            print >>f, 'ref:   ', V[conf[3]]
+            print >>f, '#'*20
+            #for feat,v in sorted(text_features[pid].items(), key=lambda t:importance(t[0],conf[2])):
+            for feat,v in sorted(text_features[pid].items(), key=lambda t:t[1]):
+                #imp = importance(feat, conf[3])
+                imp = v
+                #print >>f, feat, 
+                print >>f, '%.3f %s' % (imp,feat)
+            print >>f, '#'*20
+            print >>f, 'SCORES'
+            pind = conf[2]
+            rind = conf[3]
+            print >>f, 'predicted:', sum([val*importance(feat,pind) for feat,val in text_features[pid].items() if float('-inf')<importance(feat,pind)<float('inf')])
+            print >>f, 'true:     ', sum([val*importance(feat,rind) for feat,val in text_features[pid].items() if float('-inf')<importance(feat,rind)<float('inf')])
+            print >>f, '#'*20
+            for dt,category,text in notes[pid]:
+                print >>f, dt
+                print >>f, category
+                print >>f, text
+                print >>f, '-'*50
+            print >>f, ''
+            print >>f, '+'*80
+            print >>f, ''
+
+
+
+def softmax(scores):
+    e = np.exp(scores)
+    return e / e.sum()
+
+
+
+def make_bow(toks):
+    bow = defaultdict(int)
+    for w in toks:
+        bow[w] += 1
+    return bow
+
+
+
+def tokenize(text):
+    text = text.lower()
+    text = re.sub('[\',\.\-/\n]', ' ', text)
+    text = re.sub('[^a-zA-Z0-9 ]', '', text)
+    text = re.sub('\d[\d ]+', ' 0 ', text)
+    return text.split()
+
+
+
+def normalize_y(label, task):
+    if task == 'ethnicity':
+        if 'BLACK' in label: return 'BLACK'
+        if 'ASIAN' in label: return 'ASIAN'
+        if 'WHITE' in label: return 'WHITE'
+        if 'LATINO' in label: return 'HISPANIC'
+        if 'HISPANIC' in label: return 'HISPANIC'
+        if 'SOUTH AMERICAN' in label: return 'HISPANIC'
+        if 'PACIFIC' in label: return 'ASIAN'
+        if 'PORTUGUESE' in label: return 'WHITE'
+        if 'CARIBBEAN' in label: return 'HISPANIC'
+        if 'AMERICAN INDIAN' in label: return '**ignore**'
+        if 'ALASKA NATIVE' in label: return '**ignore**'
+        if 'MIDDLE EASTERN' in label: return '**ignore**'
+        if 'MULTI RACE' in label: return '**ignore**'
+        if 'UNKNOWN' in label: return '**ignore**'
+        if 'UNABLE TO OBTAIN' in label: return '**ignore**'
+        if 'PATIENT DECLINED TO ANSWER' in label: return '**ignore**'
+        if 'OTHER' in label: return '**ignore**'
+    return label
+
+
+
+def extract_text_features(notes, hours):
+    features = defaultdict(int)
+    features['b'] = 1.0
+
+    for note in notes:
+        dt = note[0]
+        #print dt
+        if isinstance(dt, pd._libs.tslib.NaTType): continue
+        if note[0] < datetime.timedelta(days=hours/24.0):
+            # access the note's info
+            section = note[1]
+            toks = tokenize(note[2])
+
+            bow = make_bow(toks)
+
+            #'''
+            # unigram features
+            for w,tf in bow.items():
+                if section:
+                    featname = ('unigram', w, section)
+                else:
+                    featname = ('unigram', w)
+                #features[featname] += tf
+                features[featname] += 1
+                #features[featname] = 1
+            #'''
+
+            '''
+            # ngram features
+            for n in [1,2]:
+                for i in range(len(toks)-n+1):
+                    ngram = tuple(toks[i:i+n])
+                    if section:
+                        featname = ('%d-gram'%n, ngram, section)
+                    else:
+                        featname = ('%d-gram'%n, ngram)
+                    features[featname] = 1.0
+            '''
+
+    return dict(features)
+
+
+
+if __name__ == '__main__':
+    main()
