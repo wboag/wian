13bc79ebf7724aa007dbb436b4daa1ddd40e4734
Tristan Naumann <tristan.naumann@gmail.com>
2020-05-26 14:28:10 -0700

    Updates download target
    Uses the new download string provided on https://physionet.org/content/what-is-in-a-note/0.1/.

--- resources/get_resources.sh
+++ resources/get_resources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget --user "$USER" --ask-password https://physionet.org/works/MIMICIIIDerivedDataRepository/files/approved/what-is-in-a-note/mimic10.vec.gz
+wget -r -N -c -np --user "$USER" --ask-password https://physionet.org/files/what-is-in-a-note/0.1/
 gunzip mimic10.vec.gz
